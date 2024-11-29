8ff8275e03198260e94817f0228657aa281d151c
Amir Abbas Mousavi <mamyrbas@gmail.com>
2024-11-29 23:51:06 +0330

    added simple search page ui

--- app/search.tsx
+++ app/search.tsx
@@ -1,25 +1,33 @@
+import Header from '@/src/components/header/Header';
 import { Box } from '@/src/components/ui/box';
 import { Button, ButtonText } from '@/src/components/ui/button';
+import { Heading } from '@/src/components/ui/heading';
 import { Input, InputField } from '@/src/components/ui/input';
-import { Text } from '@/src/components/ui/text';
 
 export default function SearchScreen() {
   return (
-    <Box className='flex-1 p-4 bg-white'>
-      <Text className='text-lg font-bold mb-4'>Search Movies</Text>
-      <Input size='md' className='bg-white max-w-80 rounded-none border-none'>
-        <InputField
-          placeholder='Search for a Movie...'
-          className='min-w-7 text-black'
-        />
-        <Button
-          variant='solid'
-          action='primary'
-          className='rounded-none border-none'
-        >
-          <ButtonText>Search</ButtonText>
-        </Button>
-      </Input>
+    <Box className='h-full'>
+      <Header />
+
+      <Box className='w-full h-full flex justify-center items-center pb-28'>
+        <Heading size='2xl' className='mb-4'>
+          Search a Movies
+        </Heading>
+
+        <Input size='md' className='bg-white rounded-none border-none'>
+          <InputField
+            placeholder='Search for a Movie...'
+            className='min-w-7 text-black'
+          />
+          <Button
+            variant='solid'
+            action='primary'
+            className='rounded-none border-none'
+          >
+            <ButtonText>Search</ButtonText>
+          </Button>
+        </Input>
+      </Box>
     </Box>
   );
 }
