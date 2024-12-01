582a330c1e704db244463e0039becc53879bc334
Amir Abbas Mousavi <mamyrbas@gmail.com>
2024-12-02 02:40:05 +0330

    added a very simple ErrorBoundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import { GluestackUIProvider } from '@/src/components/ui/gluestack-ui-provider';
-import { Stack } from 'expo-router';
+import { ErrorBoundaryProps, Stack } from 'expo-router';
 
 import '../src/styles/global.css';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
+import { Pressable, View } from 'react-native';
+import { Text } from '@/src/components/ui/text';
+import { LogBox } from '@/src/utils/logBox';
+
+LogBox.ignoreLogs([
+  'Warning: findDOMNode is deprecated and will be removed in the next major release.',
+]);
 
 export default function RootLayout() {
   const [colorMode] = useState<'light' | 'dark'>('dark');
@@ -19,5 +26,16 @@ export default function RootLayout() {
         }}
       />
     </GluestackUIProvider>
+  );
+}
+
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+  return (
+    <View style={{ flex: 1, margin: 50 }}>
+      <Text>{error.message}</Text>
+      <Pressable onPress={retry}>
+        <Text>Try Again?</Text>
+      </Pressable>
+    </View>
   );
 }
