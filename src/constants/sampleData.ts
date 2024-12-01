376a768083203705af7daaa534478a67daf36ef1
Amir Abbas Mousavi <mamyrbas@gmail.com>
2024-12-02 02:41:23 +0330

    made sample data a place holder

--- src/constants/sampleData.ts
+++ src/constants/sampleData.ts
@@ -1,52 +1,61 @@
 export const exampleMovieList = [
   {
-    url: '/list/1',
-    title: 'Hello',
-    image: require('@/assets/images/slide-images/purple-0.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Title for p1',
-    image: require('@/assets/images/slide-images/purple-1.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Po 2 text',
-    image: require('@/assets/images/slide-images/purple-2.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Title',
-    image: require('@/assets/images/slide-images/purple-3.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Title for p1',
-    image: require('@/assets/images/slide-images/purple-1.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Po 2 text',
-    image: require('@/assets/images/slide-images/purple-2.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
   },
   {
-    url: '/list/1',
-    title: 'Title',
-    image: require('@/assets/images/slide-images/purple-3.png'),
+    url: '',
+    title: '',
+    image: require('@/assets/images/placeholders/empty.png'),
+  },
+];
+
+export const exampleYTMovieList = [
+  {
+    key: '',
   },
   {
-    url: '/list/1',
-    title: 'Title for p1',
-    image: require('@/assets/images/slide-images/purple-1.png'),
+    key: '',
   },
   {
-    url: '/list/1',
-    title: 'Po 2 text',
-    image: require('@/assets/images/slide-images/purple-2.png'),
+    key: '',
   },
   {
-    url: '/list/1',
-    title: 'Title',
-    image: require('@/assets/images/slide-images/purple-3.png'),
+    key: '',
+  },
+  {
+    key: '',
+  },
+  {
+    key: '',
+  },
+  {
+    key: '',
   },
 ];
