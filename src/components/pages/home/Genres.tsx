065a9ffd91325f469277e8eaa91a1cc6e3d9984f
Amir Abbas Mousavi <mamyrbas@gmail.com>
2024-12-07 15:19:05 +0330

    fix: link dose'nt reset end page state so i either had to check for changes in result page or change i could use router.push  which was easer

--- src/components/pages/home/Genres.tsx
+++ src/components/pages/home/Genres.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Href, Link } from 'expo-router';
+import { router } from 'expo-router';
 import useFetch from '@/src/Hook/useFetch';
 import { TMDBService } from '@/src/services/TMDB';
 import { Box } from '../../ui/box';
 import { Text } from '../../ui/text';
 import { TMDBGenre } from '@/src/services/TMDB/types';
 import { Heading } from '../../ui/heading';
+import { Pressable } from 'react-native-gesture-handler';
 
 const Genres = () => {
   const { data, loading, error } = useFetch(() => TMDBService.getGenres());
@@ -16,6 +17,12 @@ const Genres = () => {
 
   if (!data) return;
 
+  function handlePress({ id, name }: any) {
+    router.push(
+      `search/results/?with_genres=${id}&sort_by=popularity.desc&title=${name}`
+    );
+  }
+
   return (
     <Box className='sm:m-3 md:m-6 lg:m-9 flex items-center'>
       <Heading size='2xl' className='text-center'>
@@ -24,13 +31,13 @@ const Genres = () => {
 
       <Box className='mt-5 mb-3 flex flex-row flex-wrap justify-center gap-3 max-w-[800px]'>
         {data.genres.map(({ name, id }: TMDBGenre) => (
-          <Link
-            href={`search/results/?with_genres=${id}&sort_by=popularity.desc&title=${name}`}
+          <Pressable
+            onPress={() => handlePress({ id, name })}
             key={id}
             className='border border-zinc-700 px-3 rounded-lg transition-all hover:bg-zinc-800 hover:scale-105'
           >
             <Text>{name}</Text>
-          </Link>
+          </Pressable>
         ))}
       </Box>
     </Box>
